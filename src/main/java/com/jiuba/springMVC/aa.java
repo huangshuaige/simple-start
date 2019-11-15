e746d5970eaa5b702344ea28c37b62379da073ba
hlq <814813191@qq.com>
2019-11-15 11:02:05 +0800

    一个简单的网页开始

---
+++ src/main/java/com/jiuba/springMVC/aa.java
@@ -0,0 +1,8 @@
+package com.jiuba.springMVC;
+
+/**
+ * @author huanglq
+ * @create 2019/9/12  9:49
+ **/
+public class aa {
+}
---
+++ src/main/java/com/jiuba/springMVC/controller/HellowController.java
@@ -0,0 +1,13 @@
+package com.jiuba.springMVC.controller;
+
+
+
+
+/**
+ * @author huanglq
+ * @create 2019/9/12  9:50
+ **/
+
+public class HellowController {
+
+}
